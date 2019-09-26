e3f72526a4045a19067671350034ebdc692cf841
sky94520 <541052067@qq.com>
2019-09-26 21:59:18 +0800

    主要是代理和重试机制

--- CrawlPatent/middlewares.py
+++ CrawlPatent/middlewares.py
@@ -7,111 +7,40 @@
 
 from scrapy import signals
 import proxy_pool
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
+import logging
 
 
-class CrawlpatentSpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+logger = logging.getLogger(__name__)
 
 
-class CrawlpatentDownloaderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
+class RetryOrErrorMiddleware(RetryMiddleware):
+    """在之前的基础上增加了一条判断语句，当重试次数超过阈值时，发出错误"""
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+    def _retry(self, request, reason, spider):
+        # 获取当前的重试次数
+        retry_times = request.meta.get('retry_times', 0) + 1
+        # 最大重试次数
+        max_retry_times = self.max_retry_times
+        if 'max_retry_times' in request.meta:
+            max_retry_times = request.meta['max_retry_times']
 
-    def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
-
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
-
-    def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        # 超出最大 直接报错即可
+        if retry_times > max_retry_times:
+            logger.error('%s %s retry times beyond the bounds' % (request.url, request.meta['title']))
+        super()._retry(request, reason, spider)
 
 
 class ProxyMiddleware(object):
 
     def process_request(self, request, spider):
-        # 使用代理
+        # 最大重试次数
+        max_retry_times = spider.crawler.settings.get('MAX_RETRY_TIMES')
         proxy = proxy_pool.get_random_proxy()
-        if proxy:
-            spider.logger.info('使用代理%s' % proxy)
+        # 最后一次尝试不使用代理
+        if proxy and ('retry_times' not in request.meta or request.meta['retry_times'] == max_retry_times):
+            logger.info('使用代理%s' % proxy)
             request.meta['splash']['args']['proxy'] = proxy
         else:
-            spider.logger.warning('代理获取失败，使用自己的IP')
+            logger.warning('代理获取失败，使用自己的IP')
 
