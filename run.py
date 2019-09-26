075f2e79ca151e23592d0c19dcd3eda0c630cc3b
sky94520 <541052067@qq.com>
2019-09-26 21:57:58 +0800

    目前仅仅作为scrapy的启动器

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
-import os
-import re
-import json
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
-
-
-basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-# 保存的路径
-# 队列
-queue = []
 
 
 def run():
@@ -19,34 +10,9 @@ def run():
     # 合并配置
     process = CrawlerProcess(settings)
     # 启动爬虫
-    process.crawl(spider_name, **{'queue': queue})
+    process.crawl(spider_name)
     process.start()
 
 
-def main():
-    path = os.path.join(basedir, 'files', 'page_links')
-
-    for parent, dirnames, filenames in os.walk(path, followlinks=True):
-        print(parent)
-        for filename in filenames:
-            full_filename = os.path.join(parent, filename)
-            # 工作路径
-            work_path = re.sub('page_links', 'detail', parent)
-            fp = open(full_filename, 'r', encoding='utf-8')
-            text = fp.read()
-            fp.close()
-
-            json_data = json.loads(text)
-            for datum in json_data:
-                datum['path'] = work_path
-                print(datum)
-                queue.append(datum)
-
-            run()
-            return
-            # print('文件完整路径 %s' % filepath)
-
-
 if __name__ == '__main__':
-    # run()
-    main()
+    run()
