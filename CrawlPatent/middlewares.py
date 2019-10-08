b912176eb5a635de9b3277425e46c3ad75b4e545
sky94520 <541052067@qq.com>
2019-10-08 17:11:28 +0800

    添加了从本地获取文件的middleware

--- CrawlPatent/middlewares.py
+++ CrawlPatent/middlewares.py
@@ -6,12 +6,49 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+import os
+from urllib.parse import urlsplit, parse_qsl
 import proxy_pool
+from scrapy.http import Response
+from scrapy_splash.response import SplashTextResponse
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
 import logging
 
 
 logger = logging.getLogger(__name__)
+
+
+class GetFromLocalityMiddleware(object):
+    def process_request(self, request, spider):
+        """
+        尝试从本地获取源文件，如果存在，则直接获取
+        :param request:
+        :param spider:
+        :return:
+        """
+        splash_url = spider.crawler.settings.get('SPLASH_URL')
+        splash_result = urlsplit(splash_url)
+        # 提取出code
+        url = request.url
+        result = urlsplit(url)
+        # 不进行
+        if result.scheme == splash_result.scheme and result.netloc == splash_result.netloc:
+            return None
+
+        tuple_list = parse_qsl(result.query)
+        for key, value in tuple_list:
+            if key == 'filename':
+                filename = value
+        # 文件存放位置
+        path = request.meta['path']
+        # 该路径存在该文件
+        filepath = os.path.join(path, '%s.html' % filename)
+        if os.path.exists(filepath):
+            fp = open(filepath, 'rb')
+            bytes = fp.read()
+            fp.close()
+            return SplashTextResponse(url=url, headers=request.headers, body=bytes, request=request)
+        return None
 
 
 class RetryOrErrorMiddleware(RetryMiddleware):
@@ -35,12 +72,14 @@ class ProxyMiddleware(object):
 
     def process_request(self, request, spider):
         # 最大重试次数
+        retry_times = request.meta.get('retry_times', 0)
         max_retry_times = spider.crawler.settings.get('MAX_RETRY_TIMES')
         proxy = proxy_pool.get_random_proxy()
         # 最后一次尝试不使用代理
-        if proxy and ('retry_times' not in request.meta or request.meta['retry_times'] == max_retry_times):
+        if proxy and retry_times != max_retry_times:
             logger.info('使用代理%s' % proxy)
             request.meta['splash']['args']['proxy'] = proxy
         else:
-            logger.warning('代理获取失败，使用自己的IP')
+            reason = '代理获取失败' if proxy else ('达到最大重试次数[%d/%d]' % (retry_times, max_retry_times))
+            logger.warning('%s，使用自己的IP' % reason)
 
