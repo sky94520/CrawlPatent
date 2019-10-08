a9e2c75559e054c653771c3913ef049b07673ae3
sky94520 <541052067@qq.com>
2019-10-08 17:12:40 +0800

    修正了item的获取方式

--- CrawlPatent/spiders/detail.py
+++ CrawlPatent/spiders/detail.py
@@ -32,6 +32,9 @@ class DetailSpider(scrapy.Spider):
             return splash:html()
         end
         """
+        self.pattern = re.compile(r'.*?【(.*?)】.*?')
+        # 计数器 用于统计在各个json文件中已经抓取到的链接
+        self.counter = {}
 
     def start_requests(self):
         for datum in self.get_links():
@@ -51,24 +54,23 @@ class DetailSpider(scrapy.Spider):
             # 遍历所有的文件
             for filename in filenames:
                 full_filename = os.path.join(parent, filename)
-                # 判断该文件是否已经访问过了
-                real_name = '%s/%s' % (category_code, filename)
-                if self.db.sismember('page_links', real_name):
+                # 来源文件判断该文件是否已经访问过了
+                source = '%s/%s' % (category_code, filename)
+                if self.db.sismember('page_links', source):
                     continue
-                self.db.sadd('page_links', real_name)
                 # 工作路径
                 work_path = re.sub('page_links', 'detail', parent)
                 fp = open(full_filename, 'r', encoding='utf-8')
                 text = fp.read()
                 fp.close()
-
                 json_data = json.loads(text)
+                # 计数器
+                self.counter[source] = (0, len(json_data))
+                # 解析并yield
                 for datum in json_data:
-                    datum['path'] = work_path
-                    datum['category_code'] = category_code
+                    datum.update(path=work_path, category_code=category_code, source=source)
                     yield datum
-                # TODO:当前仅仅返回一个
-                return
+                self.logger.info('File[%s] has loaded' % source)
 
     def create_request(self, top):
         args = {
@@ -79,6 +81,7 @@ class DetailSpider(scrapy.Spider):
             'path': top['path'],
             'title': top['title'],
             'category_code': top['category_code'],
+            'source': top['source'],
             'max_retry_times': self.crawler.settings.get('MAX_RETRY_TIMES')
         }
         return SplashRequest(top['url'], callback=self.parse, endpoint='execute',
@@ -88,32 +91,43 @@ class DetailSpider(scrapy.Spider):
         item = PatentItem()
         item['response'] = response
         item['title'] = response.meta['title']
+        item['source'] = response.meta['source']
         item['category_code'] = response.meta['category_code']
         try:
             # 解析页面结构
-            data = response.css('#box').css('td[bgcolor="#FFFFFF"]::text').extract()
-            data = [datum.strip() for datum in data if len(datum.strip()) > 0]
-            item['application_number'] = data[0]
-            item['application_date'] = data[1]
-            item['publication_number'] = data[2]
-            item['publication_date'] = data[3]
-            item['applicant'] = data[4]
-            item['address'] = data[5]
-            item['inventor'] = data[6]
-            item['agency'] = data[7]
-            item['agent'] = data[8]
-            item['code'] = data[9]
-            item['summary'] = data[10]
-            item['sovereignty'] = data[11]
-            item['page_number'] = data[12]
-            item['main_cls_number'] = data[13]
-            item['patent_cls_number'] = data[14]
+            tr_list = response.xpath('//table[@id="box"]/tbody/tr')
+            tr_index, tr_length = 0, len(tr_list)
+            # 去掉最后一个tr 最后一个tr
+            while tr_index < tr_length - 1:
+                td_list = tr_list[tr_index].xpath('./td')
+                tr_index += 1
+                index, length, real_key = 0, len(td_list), None
+
+                while index < length:
+                    # 提取出文本
+                    text = td_list[index].xpath('.//text()').extract_first().strip()
+                    # 已经有key，则text为对应的value
+                    if real_key:
+                        item[real_key], real_key = text, None
+                    # 过滤掉长度为0的键
+                    elif len(text) > 0:
+                        # 正则未提取到任何值 则键发生问题
+                        result = re.search(self.pattern, text)
+                        if result is None:
+                            raise
+                        key = result.group(1)
+                        # 对应的键 没有则跳过下一个
+                        if key in PatentItem.mapping:
+                            real_key = PatentItem.mapping[key]
+                        else:
+                            index += 1
+                    index += 1
             yield item
         # 页面解析错误，重试
         except Exception as e:
-            self.logger.error('%s %s页面解析出错: %s, 重试' % (response['category_code'], response['title'], e))
-            retry_times = response.meta.get('retry_times', 0) + 1
-            request = response.request.copy()
-            request.meta['retry_times'] = retry_times
-            yield request
+            self.logger.error('%s %s页面解析出错: %s, 重试' % (response.meta['category_code'], response.meta['title'], e))
+            # retry_times = response.meta.get('retry_times', 0) + 1
+            # request = response.request.copy()
+            # request.meta['retry_times'] = retry_times
+            # yield request
 
