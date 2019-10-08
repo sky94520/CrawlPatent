35af231881b8a22a1f81b695501b0f31f222bce4
sky94520 <541052067@qq.com>
2019-10-08 17:10:41 +0800

    修正了数组元素中空字符串占位的问题

--- CrawlPatent/pipelines.py
+++ CrawlPatent/pipelines.py
@@ -14,31 +14,43 @@ import logging
 from scrapy.exceptions import DropItem
 
 
+logger = logging.getLogger(__name__)
+
+
 class FilterPipeline(object):
     """清除特殊字符"""
     def __init__(self):
         # 字符串转为数组
-        self.array_keys = ['inventor', 'patent_cls_number', 'agent', 'applicant']
+        self.array_keys = ['inventor', 'patent_cls_number', 'agent', 'applicant', 'joint_applicant']
         # 字符串转为datetime
         self.date_keys = ['application_date', 'publication_date']
         # 去多个换行
         self.text_keys = ['sovereignty', 'summary']
+        self.pattern = re.compile(r'\n+')
         # 转成int
         self.int_keys = ['page_number']
-        self.pattern = re.compile(r'\n+')
 
     def process_item(self, item, spider):
-        for key, value in item.items():
-            if key in self.array_keys:
-                item[key] = value.split(';')
-            elif key in self.date_keys:
-                item[key] = datetime.datetime.strptime(value, '%Y-%m-%d')
-            elif key in self.text_keys:
-                item[key] = re.sub(self.pattern, '\n', value)
-            elif key in self.int_keys:
-                item[key] = int(value)
-        if 'response' in item:
-            del item['response']
+        try:
+            for key, value in item.items():
+                if key in self.array_keys:
+                    item[key] = []
+                    for v in value.split(';'):
+                        if len(v) > 0:
+                            item[key].append(v)
+                elif key in self.date_keys:
+                    item[key] = datetime.datetime.strptime(value, '%Y-%m-%d')
+                elif key in self.text_keys:
+                    item[key] = re.sub(self.pattern, '\n', value)
+                elif key in self.int_keys:
+                    item[key] = int(value)
+            if 'response' in item:
+                del item['response']
+        except Exception as e:
+            # 在解析时出现错误，则报错后移除该item
+            logger.error('process [%s] error: %s' % (item['publication_number'], e))
+            raise DropItem()
+
         return item
 
 
@@ -93,9 +105,18 @@ class MongoPipeline(object):
     def process_item(self, item, spdier):
         # 通过application_number保证唯一
         collection = self.db[item.collection]
-        result = collection.find_one({'application_number': item['application_number']})
+        result = collection.find_one({'application_number': item['application_number']}, {'_id': 1})
         if result is not None:
-            raise DropItem()
+            raise DropItem('the %s has already saved in database' % item['application_number'])
+        cur_count, max_count = spdier.counter[item['source']]
+        # 只有在该json文件全部链接都爬取完成的时候，才会写入到redis中
+        if cur_count + 1 >= max_count:
+            spdier.db.sadd('page_links', item['source'])
+            spdier.counter.pop(item['source'])
+        else:
+            spdier.counter[item['source']] = (cur_count + 1, max_count)
+
+        del item['source']
         self.db[item.collection].insert_one(dict(item))
         return item
 
